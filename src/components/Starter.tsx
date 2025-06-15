1a12e33b17c7ecc654b926c7335ee0b4805d4099
pongf456 <adonayyeidersonparedes@gmail.com>
2025-06-14 20:12:32 -0400

    se modificaron utilidades y se añadieron assets

--- src/components/Starter.tsx
+++ src/components/Starter.tsx
@@ -1,22 +1,35 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback } from 'react'
 import { useAppStore, Word, WordManager } from '../core'
 import WordInput from './WordInput'
 import { Icon } from '@iconify/react'
-import { AnimatePresence, motion } from 'motion/react'
+import { AnimatePresence, motion, useAnimate } from 'motion/react'
 export default function Starter() {
     const word = useAppStore((s) => s.currentWord)
+    const [scope, animate] = useAnimate<HTMLButtonElement>()
     const start = useCallback(() => {
-        useAppStore.setState({ currentWord: new Word(WordManager.getRandomWord()) })
+        if (scope.current) {
+            animate('*', {
+                opacity: 0,
+            }).then(async () => {
+                await animate(scope.current, {
+                    width: 40,
+                    height: 40,
+                },{
+                    ease:'anticipate'
+                })
+                useAppStore.setState({ currentWord: new Word(WordManager.getRandomWord()) })
+            })
+        }
+        else {
+            useAppStore.setState({ currentWord: new Word(WordManager.getRandomWord()) })
+        }
     }, [])
     return (
         <AnimatePresence mode='wait'>
             {word ? (<WordInput onSuccess={start} word={word} />) :
-                <motion.button key={'start-button'} initial={{ opacity: 0, position:'relative' }} animate={{ opacity: 1 }} exit={{
-                    opacity: 0,
-                    top: 100
-                }} onClick={start} className='flex px-2 py-1 text-2xl items-center cursor-pointer hover:bg-accent-red bg-accent-red/80 active:bg-accent-red transition-colors font-inter font-bold text-background-light rounded-md shadow-sm'>
+                <motion.button exit={{ y: 30, opacity: 0 }} ref={scope} key={'start-button'} onClick={start} className='flex px-2 py-1 text-2xl items-center cursor-pointer hover:bg-accent-red bg-accent-red/80 active:bg-accent-red transition-colors font-inter font-bold text-background-light rounded-md shadow-sm'>
                     <span>Iniciar</span>
-                    <Icon icon="mdi:play" className='text-4xl' />
+                    <Icon icon="mdi:play" className='text-4xl d' />
                 </motion.button>
             }
         </AnimatePresence>
